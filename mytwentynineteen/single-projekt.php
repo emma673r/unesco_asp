419fd0279c497a3d1e710fe5eb70b1fe79e5369f
emma673r <90177139+emma673r@users.noreply.github.com>
2022-04-27 11:04:59 +0200

    blba

--- mytwentynineteen/single-projekt.php
+++ mytwentynineteen/single-projekt.php
@@ -11,7 +11,43 @@
 
 get_header();
 ?>
+<style>
 
+
+.grid1 {
+  grid-row: 2;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-rows: repeat(auto-fit, minmax(300px, 1fr));
+}
+
+.img {
+  max-width: 400px;
+}
+
+.img {
+  grid-row:3;
+}
+
+.beskrivelse_div {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+}
+
+.kortbeskrivelse{
+grid-column: 2;
+}
+
+.beskrivelse {
+grid-column:1;
+}
+
+.extra {
+  grid-row: 5;
+}
+
+
+</style>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -21,10 +57,12 @@ get_header();
         <h3 class="navn"></h3>
         <div class="grid1">
         <img class="img" src="" alt="">
-        <p><span class="kortbeskrivelse"></span></p>
-         <p><span class="beskrivelse"></span></p>
+        <div class="beskrivelse_div">
+        <span class="kortbeskrivelse"></span>
+         <span class="beskrivelse"></span>
+        </div>
         <!-- <a href=""></a> -->
-        <p><span class="extra"></span></p>
+        <span class="extra"></span>
          </div>
 
       
